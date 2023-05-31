34ba05e5561fc5de65b822f17fb329c55a1e0831
maguibing <flymaguibing@gmail.com>
2023-05-31 22:33:24 +0800

    socket.io 基本使用

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -2,6 +2,47 @@
 import RoomStatus from '@/components/room-status.vue'
 import RoomAction from '@/components/room-action.vue'
 import RoomMessage from '@/components/room-message.vue'
+
+import type { Socket } from 'socket.io-client'
+import { io } from 'socket.io-client'
+import { onMounted, onUnmounted } from 'vue'
+// import { baseURL } from '@/utils/request'
+import { useUserStore } from '@/stores'
+import { useRoute } from 'vue-router'
+
+const store = useUserStore()
+const route = useRoute()
+
+let socket: Socket
+onUnmounted(() => {
+  socket.close()
+})
+onMounted(async () => {
+  // 建立链接，创建 socket.io 实例
+  socket = io('https://consult-api.itheima.net/', {
+    auth: {
+      token: `Bearer ${store.user?.token}`
+    },
+    query: {
+      orderId: route.query.orderId
+    }
+  })
+
+  socket.on('connect', () => {
+    // 建立连接成功
+    console.log('connect')
+  })
+
+  socket.on('error', (event) => {
+    // 错误异常消息
+    console.log('error')
+  })
+
+  socket.on('disconnect', () => {
+    // 已经断开连接
+    console.log('disconnect')
+  })
+})
 </script>
 
 <template>
